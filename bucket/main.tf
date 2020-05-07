c205ac099695eedb8af51fbc3ba2e3dc9c13594e
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-05-07 15:05:07 +0200

    name change

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "cm-test-bucket" {
-  bucket = "cm-test-bucket"
+  bucket = "cm-test-branch--bucket"
   acl = "private"
   count = "${contains(list("test", "all"), var.tag) ? 1 : 0}"
 
   tags = {
-    Name = "My CM Test Bucket"
+    Name = "My CM Test Bucket (branch test)"
     Environment = "Test"
   }
 
@@ -12,12 +12,12 @@ resource "aws_s3_bucket" "cm-test-bucket" {
 }
 
 resource "aws_s3_bucket" "cm-dev-bucket" {
-  bucket = "cm-dev-bucket"
+  bucket = "cm-dev-branch-bucket"
   acl = "private"
   count = "${contains(list("dev", "all"), var.tag) ? 1 : 0}"
 
   tags = {
-    Name = "My CM Dev Bucket"
+    Name = "My CM Dev Bucket (branch test)"
     Environment = "Dev"
   }
 }
